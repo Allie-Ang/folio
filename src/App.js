6c1cc793941854fd81d556c623cfbab69258bc99
Allison Ang <allisonang21@gmail.com>
2023-05-18 20:45:14 -0700

    Added secondary header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-     <h1>Allie Ang</h1>
+        <h1>Allie Ang</h1>
+        <h2>Full Stack Software Engineer</h2>
       </header>
     </div>
   );
