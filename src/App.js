d51c4112a3b3fd82c26a49f9ba347e8f3bc73b9a
Allison Ang <allisonang21@gmail.com>
2023-06-26 07:34:17 -0700

    editing bg code

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div
+          style={{
+            border: ".8px solid",
+            height: "100",
+            position: "absolute",
+            top: 0,
+            right: "0px",
+            bottom: "0px",
+            left: "0px",
+            margin: "26px",
+          }}
+        >
+          <h1
+            style={{
+              fontWeight: 245,
+              marginTop: 15,
+              marginLeft: 20,
+              textAlign: "left",
+              marginBottom: 0,
+            }}
+          >
+            Allie Ang
+          </h1>
+          <h2
+            style={{
+              fontWeight: 100,
+              marginLeft: 20,
+              textAlign: "left",
+              lineHeight: 0,
+              fontSize: 15,
+              marginTop: 6,
+            }}
+          >
+            Full Stack Software Engineer
+          </h2>
+          <div
+            style={{
+              textAlign: "left",
+              marginLeft: 20,
+            }}
+          >
+            <li style={{ listStyleType: "none", marginTop: 5 }}>𓆟</li>
+            <li style={{ listStyleType: "none", fontWeight: 250 }}>Projects</li>
+            <li style={{ listStyleType: "none", fontWeight: 250 }}>LinkedIn</li>
+            <li
+              style={{
+                listStyleType: "none",
+                marginBottom: 10,
+                fontWeight: 250,
+              }}
+            >
+              GitHub
+            </li>
+          </div>
+          <div style={{ border: "1px solid yellow" }}>
+            <p style={{ fontSize: "15px", lineHeight: "20px" }}>
+              I’m an ambitious software engineer with a background in
+              occupational therapy, seeking opportunities to leverage both
+              therapeutic and technical skills in a new role. Following
+              non-traditional education paths, I completed the Full-Stack
+              Software Engineer Program @ UCLA and Stanbridge University
+              receiving my degree in Occupational Therapy.
+            </p>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
